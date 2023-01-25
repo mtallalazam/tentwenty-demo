1ec701ec4491261c55dc2644fd84260acc2eab29
Muhammad Tallal Azam <m.tallalazam@gmail.com>
2023-01-25 22:13:29 +0500

    Added sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import './styles/styles.scss';
+
 ReactDOM.createRoot(document.getElementById('app')).render(
     <App />
-)
+);
